9776b774fb7c7562b9a762fb5d4cedcf104d7876
Mert <mertdemirok80@gmail.com>
2021-05-17 12:10:35 +0200

    create Note and Reciper Classes, define one-to-one relationship btwn Note and Recipe

---
+++ src/main/java/guru/springframework/domain/Note.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob // Specifies that a persistent property or field should be persisted as a large object to a database-supported large object type
+    private String recipeNotes;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
