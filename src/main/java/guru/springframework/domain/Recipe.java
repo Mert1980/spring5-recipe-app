76fb1a9172a510d26f54c9356b73b468c54537ac
Mert <mertdemirok80@gmail.com>
2021-05-18 11:16:41 +0200

    add Category class and define many-to-many relationship btwn Category and Recipe

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -29,6 +29,13 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+    private Set<Category> categories;
+
     public String getDescription() {
         return description;
     }
@@ -127,5 +134,13 @@ public class Recipe {
 
     public Long getId() {
         return id;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
