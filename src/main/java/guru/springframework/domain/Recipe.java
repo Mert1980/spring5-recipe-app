ad0db0f57b478a7006ba2dd6411b8c18efb9070b
Mert <mertdemirok80@gmail.com>
2021-05-20 14:13:10 +0200

    refactor setters and constructors

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -109,6 +109,7 @@ public class Recipe {
 
     public void setNotes(Note note) {
         this.note = note;
+        note.setRecipe(this);
     }
 
     public Difficulty getDifficulty() {
@@ -129,6 +130,12 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public void setId(Long id) {
