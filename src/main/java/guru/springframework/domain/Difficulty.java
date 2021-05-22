6683f3a0c1434aab2de95fcc0bc1f8def6abfc7b
Mert <mertdemirok80@gmail.com>
2021-05-22 13:21:08 +0200

    add Lombok annotations to all Classes

--- src/main/java/guru/springframework/domain/Difficulty.java
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -1,31 +1,22 @@
 package guru.springframework.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Id;
 
 public enum Difficulty {
+
     EASY("E"), MODERATE("M"), HARDEST("H");
 
+    @Id
     private Long id;
+
+    @Getter
+    @Setter
     private String code;
 
     Difficulty(String code) {
         this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    public Long getId() {
-        return id;
     }
 }
--- src/main/java/guru/springframework/domain/Note.java
+++ src/main/java/guru/springframework/domain/Note.java
@@ -1,9 +1,12 @@
 package guru.springframework.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 public class Note {
 
@@ -16,28 +19,4 @@ public class Note {
 
     @Lob // Specifies that a persistent property or field should be persisted as a large object to a database-supported large object type
     private String recipeNotes;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
