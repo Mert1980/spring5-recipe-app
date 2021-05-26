f79c284d4d1395cb0413ded642f2167b2a40f1a5
Mert <mertdemirok80@gmail.com>
2021-05-26 21:18:02 +0200

    test IndexController endpoints with MockMvc

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -3,21 +3,23 @@ package guru.springframework.controllers;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
-import org.codehaus.plexus.util.cli.Arg;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -37,6 +39,16 @@ public class IndexControllerTest {
         indexController = new IndexController(recipeService);
         argumentCaptor = ArgumentCaptor.forClass(Set.class);
     }
+
+    @Test
+    public void testMockMVC() throws Exception {
+       MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+       mockMvc.perform(MockMvcRequestBuilders.get("/"))
+               .andExpect(status().isOk())
+       .andExpect(view().name("index"));
+    }
+
 
     @Test
     public void getIndexPage() {
